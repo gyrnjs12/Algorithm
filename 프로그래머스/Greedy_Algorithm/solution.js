5220c5ac93b39e0d05e2e863d6550ad634fe0101
HyoKwon <rlagyrnjs12@naver.com>
2021-02-06 02:16:36 +0900

    체육복 문제 풀이

--- 프로그래머스/Greedy_Algorithm/solution.js
+++ 프로그래머스/Greedy_Algorithm/solution.js
@@ -1,13 +1,13 @@
 function solution(n, lost, reserve) {
-  var answer = 0;
+  let answer = 0;
   let arr = [];
-  for (var i = 0; i <= n; i++) {
+  for (let i = 0; i <= n; i++) {
     // 체육복 분배 시작
     arr.push(1); // 모든 학생에게 일단 체육복 1개씩 주기
     if (reserve.includes(i)) arr[i - 1]++; // 여분이 있는 학생이면 체육북 하나 추가(++)
     if (lost.includes(i)) arr[i - 1]--; // 잃어버린 학생이면 체육복 하나 없앰(--)
   } // 체육복 분배 완료
-  for (var i = 0; i < n; i++) {
+  for (let i = 0; i < n; i++) {
     // 1. 체육복 0개면 뒷 번호 친구에게 빌리기
     // 2. 뒷 번호 친구도 1개 이하밖에 없다 -> 다음 친구꺼 빌리기
     if (arr[i] === 0) {
@@ -23,11 +23,9 @@ function solution(n, lost, reserve) {
       }
     }
   }
-  for (var i = 0; i < n; i++) {
+  for (let i = 0; i < n; i++) {
     // 체육복 1개 이상인 학생 수 새기 -> 체육수업 들을 수 있는 학생 수
     if (arr[i] >= 1) answer++;
   }
-  console.log(arr);
   return answer;
 }
-console.log(solution(8, [1, 2, 4, 6], [1, 2, 4, 6]));
